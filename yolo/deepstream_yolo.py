a2f33e21970794036834c3273fba124bc3c84115
Emir Yasin Sağlam <emir_sglm1@hotmail.com>
2022-12-08 12:44:26 +0300

    update

--- yolo/deepstream_yolo.py
+++ yolo/deepstream_yolo.py
@@ -21,6 +21,7 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 ################################################################################
+#deepstream_test3_mp4_out.py
 
 import argparse
 import sys
